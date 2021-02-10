fb3e3201bcde7025f354031b1fc9eb991b8d1ecf
Kamran Ayub <kamran@hey.com>
2021-02-10 05:10:01 +0000

    adjusting hero styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -77,7 +77,6 @@ export const pageQuery = graphql`
               maxWidth: 1180
               maxHeight: 480
               resizingBehavior: PAD
-              background: "rgb:000000"
             ) {
               ...GatsbyContentfulFluid_tracedSVG
             }
