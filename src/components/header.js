811fccb6995e03df68fda84ad53760ce4373dc5f
Kamran Ayub <kamran@hey.com>
2021-02-10 04:27:27 +0000

    finish header styles

--- src/components/header.js
+++ src/components/header.js
@@ -3,11 +3,25 @@ import Container from './container'
 import Navigation from './navigation';
 import styles from './header.module.css'
 import logo from './logo.svg'
+import avatar from './avatar.svg'
 
 const Search = () => (
   <div className={styles.search}>
     <button type="submit"><i className="fa fa-search"></i></button>
     <input type="text" placeholder="Search" />
+  </div>
+)
+
+const Divider = () => (
+  <div className={styles.divider}></div>
+)
+
+const SignInButton = () => (
+  <div className={styles.signInButton}>
+    <a href="#">
+      <img src={avatar} alt="sign in" />
+      Sign-in
+    </a>
   </div>
 )
 
@@ -19,6 +33,10 @@ export default () => (
       <div className={styles.middle}>
         <Search />
         <Navigation className={styles.navigation} />
+      </div>
+      <div className={styles.end}>
+        <Divider />
+        <SignInButton />
       </div>
     </Container>
   </div>
