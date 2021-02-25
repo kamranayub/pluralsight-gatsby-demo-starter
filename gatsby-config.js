ed5fafa2d2cf7238d18bc769923062dd63a26de8
Kamran Ayub <kamran.ayub@gmail.com>
2021-02-24 22:35:52 -0600

    Add plugin options authorId and blogPathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,10 +34,12 @@ if (!spaceId || !accessToken) {
   );
 }
 
+const authorId = "15jwOBqpxqSAOy2eOO4S0m"; // YL4ATa1RwAn9uZqw29KU7 
+
 module.exports = {
   siteMetadata: {
     title: "Globomantics Engineering",
-    authorId: "15jwOBqpxqSAOy2eOO4S0m" // YL4ATa1RwAn9uZqw29KU7 
+    authorId
   },
   pathPrefix: "/gatsby-contentful-starter",
   plugins: [
--- src/plugins/blog-posts/gatsby-node.js
+++ src/plugins/blog-posts/gatsby-node.js
@@ -1,14 +1,15 @@
-const path = require("path");
-const config = require(path.resolve("./gatsby-config.js"));
-
-exports.onPreInit = ({ reporter }) =>
-  reporter.info("Initialized blog-posts plugin");
+exports.onPreInit = ({ reporter }) => reporter.info('Initialized blog-posts plugin');
 
 /**
  * Dynamically create pages for blog posts
  */
-exports.createPages = async ({ reporter, graphql, actions }) => {
-  const { createPage } = actions;
+exports.createPages = async ({ reporter, graphql, actions }, pluginOptions) => {
+  const { createPage } = actions
+  const { authorId, blogPathPrefix = '/blog' } = pluginOptions
+
+  if (!authorId) {
+    reporter.panic('A Contentful author ID is required to retrieve blog posts');
+  }
 
   const blogPost = require.resolve("./templates/blog-post.js");
   const result = await graphql(
@@ -27,7 +28,7 @@ exports.createPages = async ({ reporter, graphql, actions }) => {
       }
     `,
     {
-      authorId: config.siteMetadata.authorId,
+      authorId
     }
   );
 
@@ -40,7 +41,7 @@ exports.createPages = async ({ reporter, graphql, actions }) => {
   const posts = result.data.allContentfulBlogPost.edges;
   posts.forEach((post) => {
     createPage({
-      path: `/blog/${post.node.slug}/`,
+      path: `${blogPathPrefix}/${post.node.slug}/`,
       component: blogPost,
       context: {
         slug: post.node.slug,
