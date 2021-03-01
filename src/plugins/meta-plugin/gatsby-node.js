a91036cded0a66def242ebfd64fe24561a545ca8
Kamran Ayub <kamran.ayub@gmail.com>
2021-02-28 21:29:28 -0600

    empty gatsby-node

--- src/plugins/meta-plugin/gatsby-node.js
+++
@@ -1,7 +0,0 @@
-exports.onPreInit = ({ reporter }) => {
-  reporter.info("Initialized meta-plugin");
-  // reporter.verbose("Initialized meta-plugin");
-  // reporter.error("Error in meta-plugin", new Error('meta-plugin error'));
-  // reporter.panic("Fatal error in meta-plugin", new Error('meta-plugin error'));
-  // reporter.panicOnBuild("Fatal error during build in meta-plugin", new Error('meta-plugin error'));
-}
