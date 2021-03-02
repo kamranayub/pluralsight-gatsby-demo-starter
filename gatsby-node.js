948c333474a17ae7094dc2dc8aa10e5ac79819ba
Kamran Ayub <kamran.ayub@gmail.com>
2021-03-01 20:39:42 -0600

    Add lifecycle hooks and logging to gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,11 +2,57 @@ const Promise = require('bluebird')
 const path = require('path')
 const config = require('./gatsby-config')
 
+exports.onPreInit = ({ reporter }) => {
+  reporter.info("local-plugin lifecycle: onPreInit");
+};
+
+exports.onCreateWebpackConfig = () => {
+  console.info("local-plugin lifecycle: onCreateWebpackConfig");
+};
+
+exports.onCreateBabelConfig = () => {
+  console.info("local-plugin lifecycle: onCreateBabelConfig");
+};
+
+exports.onPreBootstrap = ({ reporter }) => {
+  reporter.info("local-plugin lifecycle: onPreBootstrap");
+};
+
+exports.onPreExtractQueries = ({ reporter }) => {
+  reporter.info("local-plugin lifecycle: onPreExtractQueries");
+};
+
+exports.onCreatePage = ({ reporter }) => {
+  
+};
+
+exports.onCreateNode = ({ reporter }) => {
+  reporter.info("local-plugin lifecycle: onCreateNode");
+};
+
+exports.onPostBootstrap = ({ reporter }) => {
+  reporter.info("local-plugin lifecycle: onPostBootstrap");
+};
+
+exports.onPreBuild = ({ reporter }) => {
+  reporter.info("local-plugin lifecycle: onPreBuild");
+};
+
+exports.onPostBuild = ({ reporter }) => {
+  reporter.info("local-plugin lifecycle: onPostBuild");
+};
+
+exports.sourceNodes = () => {
+  console.info('local-plugin lifecycle: sourceNodes')
+}
+
 /**
  * Override core pages created by Gatsby from /pages directory
  */
-exports.onCreatePage = ({ page, actions }) => {
+exports.onCreatePage = ({ page, reporter, actions }) => {
   const { createPage, deletePage } = actions
+
+  reporter.info("local-plugin lifecycle: onCreatePage");
 
   deletePage(page)
   createPage({
@@ -23,8 +69,10 @@ exports.onCreatePage = ({ page, actions }) => {
 /**
  * Dynamically create pages for blog posts
  */
-exports.createPages = ({ graphql, actions }) => {
+exports.createPages = ({ graphql, reporter, actions }) => {
   const { createPage } = actions
+
+  reporter.info('local-plugin lifecycle: createPages')
 
   return new Promise((resolve, reject) => {
     const blogPost = path.resolve('./src/templates/blog-post.js')
