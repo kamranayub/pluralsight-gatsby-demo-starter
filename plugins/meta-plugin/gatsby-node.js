e2aa51914686822ed208cdb8ab9a9675ced9fa85
Kamran Ayub <kamran.ayub@gmail.com>
2021-03-02 21:39:19 -0600

    use number for stringify

--- plugins/meta-plugin/gatsby-node.js
+++ plugins/meta-plugin/gatsby-node.js
@@ -3,14 +3,6 @@ const util = require("util");
 const writeFile = util.promisify(require("fs").writeFile);
 const exec = util.promisify(require("child_process").exec);
 
-exports.onPreInit = ({ reporter }) => {
-  reporter.info("Initialized meta-plugin");
-  // reporter.verbose("Initialized meta-plugin");
-  // reporter.error("Error in meta-plugin", new Error('meta-plugin error'));
-  // reporter.panic("Fatal error in meta-plugin", new Error('meta-plugin error'));
-  // reporter.panicOnBuild("Fatal error during build in meta-plugin", new Error('meta-plugin error'));
-};
-
 exports.onPostBuild = async ({ graphql, reporter }) => {
   const { stdout: sha } = await exec("git rev-parse HEAD");
 
@@ -30,7 +22,7 @@ exports.onPostBuild = async ({ graphql, reporter }) => {
 
   await writeFile(
     path.join("./public", "meta.json"),
-    JSON.stringify(meta, null, " ")
+    JSON.stringify(meta, null, 2)
   );
 
   reporter.info("Wrote meta.json file with build metadata");
