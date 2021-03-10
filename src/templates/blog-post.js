297432266ff69b52b0d7e4cde02237cf9498c985
Kamran Ayub <kamran.ayub@gmail.com>
2021-03-10 11:01:41 -0600

    show term usages in blog posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,20 +1,26 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import get from 'lodash/get'
-import Img from 'gatsby-image'
-import Head from '../components/head'
-import Layout from '../components/layout'
+import React from "react";
+import { graphql } from "gatsby";
+import get from "lodash/get";
+import sortBy from "lodash/sortBy";
+import reverse from "lodash/reverse";
+import Img from "gatsby-image";
+import Head from "../components/head";
+import Layout from "../components/layout";
 
-import heroStyles from '../components/hero.module.css'
+import styles from './blog-post.module.css';
+import heroStyles from "../components/hero.module.css";
 
 class BlogPostTemplate extends React.Component {
   render() {
-    const post = get(this.props, 'data.contentfulBlogPost')
-    const siteTitle = get(this.props, 'data.site.siteMetadata.title')
+    const post = get(this.props, "data.contentfulBlogPost");
+    const siteTitle = get(this.props, "data.site.siteMetadata.title");
+    const terms = reverse(
+      sortBy(get(post, "body.childGlossaryTermRefs.terms"), "count")
+    );
 
     return (
       <Layout location={this.props.location}>
-        <div style={{ background: '#fff' }}>
+        <div style={{ background: "#fff" }}>
           <Head title={`${post.title} | ${siteTitle}`} />
           <div className={heroStyles.hero}>
             <Img
@@ -27,27 +33,45 @@ class BlogPostTemplate extends React.Component {
             <h1 className="section-headline">{post.title}</h1>
             <p
               style={{
-                display: 'block',
+                display: "block",
               }}
             >
-              published {post.publishDate} by <strong>{post.author.name}</strong>
+              published {post.publishDate} by{" "}
+              <strong>{post.author.name}</strong>
             </p>
             <div
               dangerouslySetInnerHTML={{
                 __html: post.body.childMarkdownRemark.html,
               }}
             />
+            <hr />
+            <h3>Glossary</h3>
+            <dl class={styles.glossary}>
+              {terms.map(({ term }) => (
+                <>
+                  <dt>
+                    {term.abbreviation} ({term.name})
+                  </dt>
+                  <dd>{term.description}</dd>
+                </>
+              ))}
+            </dl>
           </div>
         </div>
       </Layout>
-    )
+    );
   }
 }
 
-export default BlogPostTemplate
+export default BlogPostTemplate;
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
     contentfulBlogPost(slug: { eq: $slug }) {
       title
       publishDate(formatString: "MMMM Do, YYYY")
@@ -60,10 +84,20 @@ export const pageQuery = graphql`
         childMarkdownRemark {
           html
         }
+        childGlossaryTermRefs {
+          terms {
+            count
+            term {
+              abbreviation
+              name
+              description
+            }
+          }
+        }
       }
       author {
         name
       }
     }
   }
-`
+`;
