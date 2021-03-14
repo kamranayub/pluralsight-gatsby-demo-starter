289f9bac5f11fc159681a3bf3942619c82ee2fca
Kamran Ayub <kamran.ayub@gmail.com>
2021-03-13 22:59:44 -0600

    Add shadowing for header

---
+++ src/@kamranayub/gatsby-theme-globomantics-blog/components/header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "@kamranayub/gatsby-theme-globomantics-blog/src/components/header";
+
+import styles from './header.marketing.module.css'
+
+export default () => (
+  <Header className={styles.wrapper} tagline="MARKETING BLOGS" />
+);
