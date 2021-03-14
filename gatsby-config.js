37e66c944e70d9c306b3a86f0ac19d5fc2c62414
Kamran Ayub <kamran.ayub@gmail.com>
2021-03-13 21:28:00 -0600

    Update starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   plugins: [
     {
-      resolve: "@kamranayub/globomantics-blog-theme",
+      resolve: "@kamranayub/gatsby-theme-globomantics-blog",
       options: {
+        title: "Globomantics Engineering",
         authorId: "15jwOBqpxqSAOy2eOO4S0m", // YL4ATa1RwAn9uZqw29KU7
         title: "Globomantics Engineering",
       },
