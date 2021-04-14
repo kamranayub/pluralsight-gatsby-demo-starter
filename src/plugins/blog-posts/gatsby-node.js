b4b4cd50e8b282c87fad3b63d47ee32d678c7d16
Kamran Ayub <kamran.ayub@gmail.com>
2021-04-13 23:10:47 -0500

    formatting

--- src/plugins/blog-posts/gatsby-node.js
+++ src/plugins/blog-posts/gatsby-node.js
@@ -1,15 +1,12 @@
-exports.onPreInit = ({ reporter }) => reporter.info('Initialized blog-posts plugin');
+exports.onPreInit = ({ reporter }) =>
+  reporter.info("Initialized blog-posts plugin");
 
 /**
  * Dynamically create pages for blog posts
  */
 exports.createPages = async ({ reporter, graphql, actions }, pluginOptions) => {
-  const { createPage } = actions
-  const { authorId, blogPathPrefix = '/blog' } = pluginOptions
-
-  if (!authorId) {
-    reporter.panic('A Contentful author ID is required to retrieve blog posts');
-  }
+  const { createPage } = actions;
+  const { authorId, blogPathPrefix = "/blog" } = pluginOptions;
 
   const blogPost = require.resolve("./templates/blog-post.js");
   const result = await graphql(
@@ -28,7 +25,7 @@ exports.createPages = async ({ reporter, graphql, actions }, pluginOptions) => {
       }
     `,
     {
-      authorId
+      authorId,
     }
   );
 
