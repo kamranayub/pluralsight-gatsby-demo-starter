33e98d6117310e07857ccddda2a203cf169505fe
Kamran Ayub <kamran@hey.com>
2021-04-13 23:29:34 -0500

    Module 3: onPostBuild (#8)

--- plugins/meta-plugin/gatsby-node.js
+++ plugins/meta-plugin/gatsby-node.js
@@ -1,8 +1,30 @@
-exports.onPreInit = ({ reporter }) => {
-  reporter.info("Initialized meta-plugin");
-  // reporter.verbose("Initialized meta-plugin");
-  // reporter.warn("Warning in meta-plugin");
-  // reporter.error("Error in meta-plugin", new Error('meta-plugin error'));
-  // reporter.panic("Fatal error in meta-plugin", new Error('meta-plugin error'));
-  // reporter.panicOnBuild("Fatal error during build in meta-plugin", new Error('meta-plugin error'));
-}
+const path = require("path");
+const util = require("util");
+const writeFile = util.promisify(require("fs").writeFile);
+const exec = util.promisify(require("child_process").exec);
+
+exports.onPostBuild = async ({ graphql, reporter }) => {
+  const { stdout: sha } = await exec("git rev-parse HEAD");
+  const branch = process.env.BRANCH;
+  
+  const { data } = await graphql(`
+    {
+      siteBuildMetadata {
+        buildTime
+      }
+    }
+  `);
+
+  const meta = {
+    sha: sha?.trim(),
+    branch,
+    buildTime: data.siteBuildMetadata.buildTime,
+  };
+
+  await writeFile(
+    path.join("./public", "meta.json"),
+    JSON.stringify(meta, null, 2)
+  );
+
+  reporter.info("Wrote meta.json file with build metadata");
+};
