b87692c9ec5d250f42a10d0a857c1601a9be8b16
Kamran Ayub <kamran@hey.com>
2021-04-13 23:07:08 -0500

    Module 3: createPages API (#9)

---
+++ src/plugins/blog-posts/gatsby-node.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const config = require(path.resolve("./gatsby-config.js"));
+
+exports.onPreInit = ({ reporter }) =>
+  reporter.info("Initialized blog-posts plugin");
+
+/**
+ * Dynamically create pages for blog posts
+ */
+exports.createPages = async ({ reporter, graphql, actions }) => {
+  const { createPage } = actions;
+
+  const blogPost = require.resolve("./templates/blog-post.js");
+  const result = await graphql(
+    `
+      query AuthorPosts($authorId: String) {
+        allContentfulBlogPost(
+          filter: { author: { contentful_id: { eq: $authorId } } }
+        ) {
+          edges {
+            node {
+              title
+              slug
+            }
+          }
+        }
+      }
+    `,
+    {
+      authorId: config.siteMetadata.authorId,
+    }
+  );
+
+  if (result.errors) {
+    console.log(result.errors);
+    reporter.panic("Could not retrieve blog posts");
+    return;
+  }
+
+  const posts = result.data.allContentfulBlogPost.edges;
+  posts.forEach((post) => {
+    createPage({
+      path: `/blog/${post.node.slug}/`,
+      component: blogPost,
+      context: {
+        slug: post.node.slug,
+      },
+    });
+  });
+
+  reporter.info(`Generated ${posts.length} blog posts using template`);
+};
