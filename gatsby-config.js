01e4da11b66ba66b79598e9af24945719a57e077
Kamran Ayub <kamran.ayub@gmail.com>
2021-04-19 08:24:26 -0500

    update title

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       resolve: "@kamranayub/gatsby-theme-globomantics-blog",
       options: {
-        title: "Engineering at Tiffany's",
+        title: "Globomantics Starter Blog",
         authorId: "15jwOBqpxqSAOy2eOO4S0m", // Anil: YL4ATa1RwAn9uZqw29KU7
       },
     },
